551a54f30671154e483f8f922ae5793675c65d2f
Shamil GHASEETA <shamil.ghaseeta@gmail.com>
2026-04-16 22:32:04 +0200

    feat: files and git tools module

---
+++ src/tuleap_mcp/tools/files.py
@@ -0,0 +1,6 @@
+from typing import List, Dict, Any
+from ..client import TuleapClient
+
+async def get_git_repositories(client: TuleapClient, project_id: int) -> List[Dict[str, Any]]:
+    """List Git repositories in a project."""
+    return await client.get(f"/projects/{project_id}/git")
